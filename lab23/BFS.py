f6d3d175107ba077d78f34e195df1967871f038b
Robert <roaltm@ttu.ee>
2020-02-11 20:50:53 +0200

    BFS working, finds diamond and shows path to it.

--- lab23/BFS.py
+++ lab23/BFS.py
@@ -2,41 +2,49 @@ from queue import Queue
 
 
 def minu_otsing(kaart, start):
-
     frontier = Queue()
     frontier.put(start)
-    visited_coords = {}
+    came_from = {start: None}
+    diamond_coordinates = ()
 
     while not frontier.empty():
         current = frontier.get()
         current_x = current[0]
         current_y = current[1]
         print(current_x, current_y)
-
         print(kaart[current_x][current_y])
         if kaart[current_x][current_y] == "D":
-            print("Jõudsin lõppsihtkohta")
+            diamond_coordinates = current
             break
 
-        if check_conditions(current_x + 1, current_y, kaart, visited_coords):
-            frontier.put((current_x + 1, current_y))
-
-        if check_conditions(current_x - 1, current_y, kaart, visited_coords):
-            frontier.put((current_x - 1, current_y))
-
-        if check_conditions(current_x, current_y - 1, kaart, visited_coords):
-            frontier.put((current_x, current_y - 1))
-
-        if check_conditions(current_x, current_y + 1, kaart, visited_coords):
-            frontier.put((current_x, current_y + 1))
-
-        visited_coords[current_x] = current_y
+        neighbor_list = find_neighbors(current_x, current_y)
+        for neighbor in neighbor_list:
+            if check_conditions(neighbor, kaart, came_from):
+                frontier.put(neighbor)
+                came_from[neighbor] = current
+    return get_path(came_from, diamond_coordinates)
 
 
-def check_conditions(current_x, current_y, kaart, visited_coords):
-    return current_x <= len(kaart) - 1 and current_y <= len(kaart[0]) - 1\
-           and current_x not in list(visited_coords.keys()) and current_y not in list(visited_coords.values())\
-           and current_x >= 0 and current_y >= 0
+def get_path(came_from, diamond_coordinates):
+    path = [diamond_coordinates, came_from[diamond_coordinates]]
+    while came_from[diamond_coordinates] is not None:
+        diamond_coordinates = came_from[diamond_coordinates]
+        path.append(diamond_coordinates)
+    return path
+
+
+def find_neighbors(current_x, current_y):
+    return [(current_x + 1, current_y), (current_x - 1, current_y),
+            (current_x, current_y + 1), (current_x, current_y - 1)]
+
+
+def check_conditions(current, kaart, came_from):
+    current_x = current[0]
+    current_y = current[1]
+    if current not in came_from and current_x <= len(kaart) - 1 and current_y <= len(kaart[0]) - 1 and current_x >= 0 and current_y >= 0:
+        return kaart[current_x][current_y] != "*"
+    else:
+        return False
 
 
 if __name__ == "__main__":
@@ -59,4 +67,4 @@ if __name__ == "__main__":
     ]
     start_row = 14
     start_col = 16
-    minu_otsing(lava_map1, (start_row, start_col))
+    print(minu_otsing(lava_map1, (start_row, start_col)))
