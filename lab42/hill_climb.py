77865ef79ba4e1f05de3b271e953faccd8ad3c45
Robert <roaltm@ttu.ee>
2020-03-03 20:30:38 +0200

    Added random move selection from best moves. Added time measurement

--- lab42/hill_climb.py
+++ lab42/hill_climb.py
@@ -1,22 +1,30 @@
+import random, time
 
-def hill_climbing(pos):
-    curr_value = pos.value()
+def hill_climbing(position):
+    current_value = position.value()
     while True:
-        move, new_value = pos.best_move()
-        print(pos.best_move())
-        if new_value >= curr_value:
+        move, new_value = position.best_move()
+        if new_value >= current_value:
             # no improvement, give up
-            return pos, curr_value
+            return position, current_value
         else:
             # position improves, keep searching
-            curr_value = new_value
-            pos.make_move(move)
+            current_value = new_value
+            print("Made move: ", move, new_value)
+            position.make_move(move)
+            print(position.queenList)
 
 
 class NQPosition:
+    def __str__(self) -> str:
+        return str(self.queenList)
+
     def __init__(self, N):
         self.N = N
         self.queenList = []
+        self.previousX = 9999
+        self.previousY = 9999
+        self.move_number = 0
         for i in range(N):
             self.queenList.append(0)
 
@@ -27,9 +35,8 @@ class NQPosition:
             for j in range(i + 1, self.N):
                 if self.queenList[i] == self.queenList[j]:
                     conflict_number += 1
-                if self.queenList[i] == self.queenList[j] + 1:
-                    conflict_number += 1
-                if self.queenList[i] == self.queenList[j] - 1:
+                offset = j - i
+                if self.queenList[i] == self.queenList[j] - offset or self.queenList[i] == self.queenList[j] + offset:
                     conflict_number += 1
         return conflict_number
 
@@ -37,7 +44,14 @@ class NQPosition:
     def make_move(self, move):
         xCoord = move[0]
         yCoord = move[1]
+        self.previousX = xCoord
+        self.previousY = self.queenList[xCoord]
+        self.move_number += 1
         self.queenList[xCoord] = yCoord
+
+    def revert_move(self):
+        self.queenList[self.previousX] = self.previousY
+        self.move_number -= 1
 
 
     def best_move(self):
@@ -45,22 +59,25 @@ class NQPosition:
         for i in range(self.N):
             for j in range(1, self.N):
                 possible_moves.append((i, self.queenList[i] + j))
-
         best_conflict_value = 99999
-        best_move = ()
+        best_moves = []
         for move in possible_moves:
-            nqPosition = NQPosition(self.N)
-            nqPosition.make_move(move)
-            conflict_value = nqPosition.value()
+            self.make_move(move)
+            conflict_value = self.value()
             if conflict_value < best_conflict_value:
                 best_conflict_value = conflict_value
-                best_move = move
+                best_moves = [move]
+            elif conflict_value == best_conflict_value:
+                best_moves.append(move)
+            self.revert_move()
+        return best_moves[random.randint(0, len(best_moves) - 1)], best_conflict_value
 
-        return best_move, best_conflict_value
 
-
-pos = NQPosition(4)
-print("Initial position value", pos.value())
-best_pos, best_value = hill_climbing(pos)
-# print("Final value", best_value)
-# if best_value is 0, we solved the problem
+position = NQPosition(4)
+print("Initial position conflict number:", position.value())
+start_time = time.time() * 1000
+best_pos, best_value = hill_climbing(position)
+end_time = time.time() * 1000
+print("Final position: ", best_pos)
+print("Final value", best_value)
+print("Time elapsed in milliseconds: ", end_time - start_time)
