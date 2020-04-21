51af3a733c9208d45eab63517f3df2abc609b279
Robert <roaltm@ttu.ee>
2020-04-22 00:23:13 +0300

    Added message classification and two spam messages. Algorithm done

--- lab91/spam_classifier.py
+++ lab91/spam_classifier.py
@@ -23,23 +23,69 @@ def stopword(wstr):
     return False
 
 
+def read_message(message_file):
+    words = []
+    f = open(message_file, "r")
+    for line in f:
+        for word in line.split():
+            words.append(word)
+    return words
+
+
 def count_directory_words(directory_messages):
     words = []
     for message in directory_messages:
         for word in message:
             words.append(word)
-    return len(words), len(set(words))
+    return words
 
 
 def train_function(ham_l, spam_l):
-    ham_words = count_directory_words(ham_l)
-    ham_word_amount = ham_words[0]
-    ham_unique_word_amount = ham_words[1]
+    """Gets data based on spam and ham messages."""
+    ham_word_data = count_directory_words(ham_l)
+    spam_word_data = count_directory_words(spam_l)
+    return ham_word_data, spam_word_data
 
-    spam_words = count_directory_words(spam_l)
-    spam_word_amount = spam_words[0]
-    spam_unique_word_amount = spam_words[1]
-    print(spam_word_amount, spam_unique_word_amount)
+
+def count_words(word, words):
+    same_words_in_message = 0
+    for element in words:
+        if element == word:
+            same_words_in_message += 1
+    return same_words_in_message
+
+
+def calculate_word_probability(word, data_words):
+    same_word_count = count_words(word, data_words)
+    data_word_count = len(data_words)
+    data_unique_word_count = len(set(data_words))
+    return (same_word_count + 1) / (data_word_count + data_unique_word_count)
+
+
+def get_message_probability(message_words, data_words):
+    message_probability = 0.0
+    for word in message_words:
+        message_probability += calculate_word_probability(word, data_words)
+    return message_probability
+
+
+def classify_message(message_words, ham_l, spam_l):
+    """Calculates if message is spam or ham based on probability comparison."""
+    data_ham_words, data_spam_words = train_function(ham_l, spam_l)
+    message_unique_words = set(message_words)
+    message_ham_words, message_spam_words = [], []
+    for word in message_unique_words:
+        if word in data_ham_words:
+            message_ham_words.append(word)
+        if word in data_spam_words:
+            message_spam_words.append(word)
+    probability_ham = ((len(ham_l)) / (len(ham_l) + len(spam_l))) + get_message_probability(message_ham_words, data_ham_words)
+    probability_spam = ((len(spam_l)) / (len(ham_l) + len(spam_l))) + get_message_probability(message_spam_words, data_spam_words)
+    print(probability_ham, probability_spam)
+    if probability_ham > probability_spam:
+        return "This letter is ham."
+    else:
+        return "This letter is spam."
 
 
 if __name__ == '__main__':
@@ -47,5 +93,8 @@ if __name__ == '__main__':
     spam_letters = read_directory(os.path.join("enron6", "spam"))
     print(len(ham_letters), "ham messages")
     print(len(spam_letters), "spam messages")
+    message1_words = read_message("message1.txt")
+    message2_words = read_message("message2.txt")
 
-    train_function(ham_letters, spam_letters)
+    print(classify_message(message1_words, ham_letters, spam_letters))
+    print(classify_message(message2_words, ham_letters, spam_letters))
