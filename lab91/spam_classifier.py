79f4cfd92a8b7d2fb41fa21051bacbf59906998d
Robert <roaltm@ttu.ee>
2020-04-21 23:04:21 +0300

    Added training function with ham and spam letters

--- lab91/spam_classifier.py
+++ lab91/spam_classifier.py
@@ -1,7 +1,6 @@
 """This algorithm detects spam email from ham email. It is based on Bayes classifier."""
 
 import os
-import pathlib
 
 
 def read_directory(dirn):
@@ -17,15 +16,36 @@ def read_directory(dirn):
 
 
 def stopword(wstr):
+    """Function checks if word is not shorter than 4 characters."""
     w = wstr.strip()
     if len(w) < 4:
         return True
     return False
 
 
-if __name__ == '__main__':
-    ham_l = read_directory(os.path.join("enron6", "ham"))
-    spam_l = read_directory(os.path.join("enron6", "spam"))
+def count_directory_words(directory_messages):
+    words = []
+    for message in directory_messages:
+        for word in message:
+            words.append(word)
+    return len(words), len(set(words))
 
-    print(len(ham_l), "ham messages")  # meaning, these are not spam
-    print(len(spam_l), "spam messages")
+
+def train_function(ham_l, spam_l):
+    ham_words = count_directory_words(ham_l)
+    ham_word_amount = ham_words[0]
+    ham_unique_word_amount = ham_words[1]
+
+    spam_words = count_directory_words(spam_l)
+    spam_word_amount = spam_words[0]
+    spam_unique_word_amount = spam_words[1]
+    print(spam_word_amount, spam_unique_word_amount)
+
+
+if __name__ == '__main__':
+    ham_letters = read_directory(os.path.join("enron6", "ham"))
+    spam_letters = read_directory(os.path.join("enron6", "spam"))
+    print(len(ham_letters), "ham messages")
+    print(len(spam_letters), "spam messages")
+
+    train_function(ham_letters, spam_letters)
