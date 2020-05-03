e2e73548e67aeee28d04793f776e9bc1a94e9717
Robert <roaltm@ttu.ee>
2020-05-03 18:13:38 +0300

    Added calculation of entropy, algorithm works now

--- lab113/zoo.py
+++ lab113/zoo.py
@@ -1,3 +1,4 @@
+import math
 import pandas as pd
 
 features = ["hair", "feathers", "eggs", "milk", "airborne",
@@ -9,5 +10,27 @@ zoo_data = pd.read_csv("zoo.data",
     names = ["name"] + features + ["type"])
 
 
+def get_samples_by_parameter(parameter, cur_type, value):
+    for key, val in zoo_data.groupby([parameter, "type"]).size().items():
+        if key == (value, cur_type):
+            return val
+
+
+def calculate_entropy(features, zoo_data):
+    for feature in features:
+        N = zoo_data[feature].size
+        H = 0
+        for key, val in zoo_data.groupby([feature]).size().items():
+            Hi = 0
+            Ni = val
+            for key1, val1 in zoo_data.groupby(["type"]).size().items():
+                Nij = get_samples_by_parameter(feature, key1, key)
+                if Nij:
+                    Pij = Nij / Ni
+                    Hi -= Pij * math.log2(Pij)
+            H += (Ni * Hi) / N
+        print(feature + ' entropy = %.4f' % H)
+
+
 if __name__ == '__main__':
-    print(zoo_data)
+    calculate_entropy(features, zoo_data)
