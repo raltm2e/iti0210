cfbfd44512acef24192d5b22d4dee7612057b587
Robert <roaltm@ttu.ee>
2020-05-12 22:56:43 +0300

    Implemented q_learning, but it has some bugs, starts jerking after learning

--- lab142/balancing.py
+++ lab142/balancing.py
@@ -1,33 +1,88 @@
+import random
+
 import gym
 
-env = gym.make('CartPole-v0')
+"""        
+        Observation:
+        Type: Box(4)
+        Num	Observation               Min             Max
+        0	Cart Position             -4.8            4.8
+        1	Cart Velocity             -Inf            Inf
+        2	Pole Angle                -24 deg         24 deg
+        3	Pole Velocity At Tip      -Inf            Inf
+        
+        Actions:
+        Type: Discrete(2)
+        Num	Action
+        0	Push cart to the left
+        1	Push cart to the right
+"""
 
-for i_episode in range(200):
-    observation = env.reset()
-    q_values = [[]]
-    for t in range(200):
-        env.render()
 
-        # juhuslik action, selle asemel tee q-tabelist valimine epsilon-greedy põhimõttel
-        action = env.action_space.sample()
+def get_best_move(q_table):
+    biggest_value = -999999
+    correct_key = ()
+    for key in q_table:
+        if q_table[key] > biggest_value:
+            biggest_value = q_table[key]
+            correct_key = key
+    if len(correct_key) == 0:
+        return random.choice([0, 1])
+    # if cart is going left
+    if correct_key[0] < 0:
+        return 0
+    else:
+        return 1
 
-        next_observation, reward, done, info = env.step(action)
-        if done:
-            reward = -reward   # simulatsioon lõppes enne 200 sammu, negatiivne tasu
 
-        # treeni siin Q-tabelit
-        learning_rate = 0.1
-        gamma = 1
-        #  Q[s,a] += learning_rate * (r + gamma * Q[s',a'] - Q[s,a])
-        #  s: observation
-        #  a: action
-        #  r: reward
-        #  s': next_observation
-        #  a': parim action olekus s',
-        #      pead ise leidma Q tabelist suurima väärtuse
+def get_move(epsilon, q_table):
+    # Return either 0 (left) or 1 (right)
+    if random.random() > epsilon:
+        return get_best_move(q_table)
+    else:
+        return random.choice([0, 1])
 
-        observation = next_observation
-        if done:
-            print("Episode {} finished after {} timesteps".format(i_episode, t+1))
-            break
-env.close()
+
+def main(learning_rate, gamma, epsilon):
+    env = gym.make('CartPole-v0')
+    q_table = {}
+
+    for i_episode in range(200):
+        observation = env.reset()
+        for t in range(200):
+            env.render()
+            action = get_move(epsilon, q_table)
+
+            next_observation, reward, done, info = env.step(action)
+            if done:
+                reward = -reward  # simulatsioon lõppes enne 200 sammu, negatiivne tasu
+
+            table_key = (round(observation[1], 2), round(observation[2], 2))  # Cart velocity and Pole angle
+            next_table_key = (round(next_observation[1], 2), round(next_observation[2], 2))
+
+            if table_key in q_table.keys():
+                if next_table_key in q_table.keys():
+                    q_table[table_key] += learning_rate * (
+                                reward + gamma * q_table[next_table_key] - q_table[table_key])
+                else:
+                    q_table[table_key] += learning_rate * reward
+            else:
+                q_table[table_key] = learning_rate * reward
+
+            # Q[s,a] += learning_rate * (reward + gamma * Q[s',a'] - Q[s,a])
+
+            observation = next_observation
+            if done:
+                print("Episode {} finished after {} timesteps".format(i_episode, t + 1))
+                print(q_table)
+                break
+        if i_episode % 10 == 0 and epsilon > 0.1:
+            epsilon -= 0.1
+    env.close()
+
+
+if __name__ == '__main__':
+    learning_rate = 0.1
+    gamma = 1
+    epsilon = 1  # Randomness chance when choosing an action
+    main(learning_rate, gamma, epsilon)
