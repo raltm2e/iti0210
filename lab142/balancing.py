4d2ab665127b48f5722a6509baec33c82d15da26
Robert <roaltm@ttu.ee>
2020-05-13 10:54:09 +0300

    Refactored some code

--- lab142/balancing.py
+++ lab142/balancing.py
@@ -18,20 +18,20 @@ import gym
         1	Push cart to the right
 """
 
+increment = 4
+angle_increment = 10
+
 def get_key(observation):
-    cart_velocity = round(observation[1] * 4) / 4  # Grouping measurements by 0.2 increments
-    pole_angle = round(observation[2] * 4) / 4
-    # if cart_velocity == -0.0:
-    #     cart_velocity = 0.0
-    # if pole_angle == -0.0:
-    #     pole_angle = 0.0
+    cart_velocity = round(observation[1] * increment) / increment  # Grouping measurements by 0.2 increments
+    pole_angle = round(observation[2] * angle_increment) / angle_increment
     returnable_tuple = (cart_velocity, pole_angle)
     return returnable_tuple
 
 
 def get_best_move(q_table, observation):
-    angle = round(observation[2] * 4) / 4
+    angle = round(observation[2] * angle_increment) / angle_increment
     for key in q_table:
+        # If there is a key with this angle, get the key's velocity and return either 0 (left) or 1 (right)
         if key[1] == angle and q_table[key] > 1:
             if key[0] <= 0:
                 return 0
@@ -58,14 +58,12 @@ def main(learning_rate, gamma, epsilon):
         for t in range(200):
             env.render()
             action = get_move(epsilon, q_table, observation)
-
             next_observation, reward, done, info = env.step(action)
             if done:
                 reward = -reward  # simulatsioon lõppes enne 200 sammu, negatiivne tasu
 
             table_key = get_key(observation)  # Cart velocity and Pole angle
             next_table_key = get_key(next_observation)
-
             if table_key in q_table.keys():
                 if next_table_key in q_table.keys():
                     q_table[table_key] += learning_rate * (reward + gamma * q_table[next_table_key] - q_table[table_key])
@@ -88,7 +86,7 @@ def main(learning_rate, gamma, epsilon):
 
 
 if __name__ == '__main__':
-    learning_rate = 0.2
+    learning_rate = 0.1
     gamma = 1
     epsilon = 1  # Randomness chance when choosing an action
     main(learning_rate, gamma, epsilon)
